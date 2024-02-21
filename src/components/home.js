a0d7d9774bcb9280cc30c05f807327d2ea6b4aa3
teyvi <44750503+teyvi@users.noreply.github.com>
2024-02-21 05:39:04 +0000

    created home and navbar, imported icons, created asset folder

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import navbar from './navbar'
+
+const home = () => {
+  return <div className='home-container'>
+        <navbar/>
+        </div>
+  
+}
+
+export default home
---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Logo from "../Assets/Logo.svg";
+import {BsCart2} from "react-icons/bs";
+
+const navbar = () => {
+  return <div>Navbar</div>
+};
+
+export default navbar;
